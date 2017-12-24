4b536f70c039df004359bf978c1fa8c9f4f45b3d
marufhossain88 <hossainmaruf88@gmail.com>
2017-12-24 13:15:27 +0600

    new file

---
+++ Example.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Web App Develop-PHP
+ * Date: 12/24/2017
+ * Time: 1:11 PM
+ */
+class Example
+{
+
+}
---
+++ syntax-php-16.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Web App Develop-PHP
+ * Date: 12/24/2017
+ * Time: 9:06 AM
+ */
